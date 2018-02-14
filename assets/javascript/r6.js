3d6d6b5477f8076e812af329fc845b7e356fb23c
Nathan Geckle <nathan_geckle@yahoo.com>
2018-02-14 01:45:35 -0500

    Finished attacker arrays

---
+++ assets/javascript/r6.js
@@ -0,0 +1,154 @@
+//Arrays
+var attackOps = ["Ash", "Sledge", "Thatcher", "Thermite", "Twitch", "Montagne", "Glaz", "Fuze", "Blitz", "IQ", "Buck", "Blackbeard", "Capitao", "Hibana", "Jackal", "Ying", "Zophia", "Dokkaebi"];
+
+var defendOps = ["Smoke", "Mute", "Castle", "Pulse", "Doc", "Rook", "Kapkan", "Tachanka", "Jager", "Bandit", "Frost", "Valkyrie", "Caveira", "Echo", "Mira", "Lesion", "Ela", "Vigil"];
+
+var recruitAttack = ["SASA", "FBIA", "GIGNA", "SpetsnazA", "GSG9A"];
+
+var recruitDefend = ["SASD", "FBID", "GIGND", "SpetsnazD", "GSG9D"];
+//Guns and equipment
+var ashPrimary = ["G36C", "R4-C"];
+var ashSecondary = ["M45 MEUSOC", "5.7 USG"];
+var ashEquipment = ["Breach Charge", "Flash Grenade"];
+
+var sledgePrimary = ["M590A1", "L85A2"];
+var sledgeSecondary = ["P226 MK 25", "SMG-11"];
+var sledgeEquipment = ["Frag Grenade", "Flash Grendade"];
+
+var thatcherPrimary = ["AR33", "M590A1", "L85A2"];
+var thatcherSecondary =["P226 MK 25"];
+var thatcherEquipment = ["Breach Charge", "Claymore"];
+
+var thermitePrimary = ["M1014", "556XI"];
+var thermiteSecondary = ["M45 MEUSOC", "5.7 USG"];
+var thermiteEquiment = ["Claymore", "Flash Grenade"];
+
+var twitchPrimary = ["F2", "417", "SG-CQB"];
+var twitchSecondary = ["P9", "LFP586"];
+var twitchEquipment = ["Claymore", "Breach Charge"];
+
+var montagnePrimary = ["Extendable Shield"];
+var montagneSecondary = ["P9", "LFP586"];
+var montagneEquipment = ["Flash Grenade", "Smoke Grenade"];
+
+var glazPrimary = ["OTs-03"];
+var glazSecondary = ["GSH-18", "PMM"];
+var glazEquipment = ["Smoke Grenade", "Claymore"];
+
+var fuzePrimary = ["Ballistic Shield", "6P41", "AK-12"];
+var fuzeSecondary = ["GSH-18", "PMM"];
+var fuzeEquipment = ["Breach Charge", "Smoke Grenade"];
+
+var blitzPrimary = ["Flash Shield"];
+var blitzSecondary = ["P12"];
+var blitzEquipment = ["Breach Charge", "Smoke Grenade"];
+
+var iqPrimary = ["AUG A2", "552 Commando", "G8A1"];
+var iqSecondary = ["P12"];
+var iqEquipment = ["Frag Grenade", "Breach Charge"];
+
+var buckPrimary = ["C8-SFW", "CAMRS"];
+var buckSecondary = ["MK1 9MM"];
+var buckEquipment = ["Frag Grenade", "Flash Grenade"];
+
+var blackbeardPrimary = ["MK17 CQB", "SR-25"];
+var blackbeardSecondary = ["D-50"];
+var blackbeardEquipment = ["Breach Charge", "Flash Grenade"];
+
+var capitaoPrimary = ["PARA-308", "M249"];
+var capitaoSecondary = ["PRB92"];
+var capitaoEquipment = ["Claymore", "Flash Grenade"];
+
+var hibanaPrimary = ["Type-89", "Supernova"];
+var hibanaSecondary = ["P229", "Bearing 9"];
+var hibanaEquipment = ["Flash Grenade", "Claymore"];
+
+var jackalPrimary = ["C7E", "PDW9", "ITA12L"];
+var jackalSecondary = ["ITA12S", "USP40"];
+var jackalEquipment = ["Breach Charge", "Smoke Grenade"];
+
+var yingPrimary = ["T-95 LSW", "SIX12"];
+var yingSecondary = ["Q-929"];
+var yingEquipment = ["Breach Charge", "Smoke Grenade"];
+
+var zophiaPrimary = ["LMG-E", "M762"];
+var zophiaSecondary = ["RG15"];
+var zophiaEquipment = ["Breach Charge", "Claymore"];
+
+var dokkaebiPrimary = ["MK14 EBR", "BOSG 12.2"];
+var dokkaebiSecondary = ["C75 Auto", "SMG-12"];
+var dokkaebiEquipment = ["Smoke Grenade", "Claymore"];
+
+var smokePrimary
+var smokeSecondary
+var smokeEquipment
+
+var mutePrimary
+var muteSecondary
+var muteEquipment
+
+var castlePrimary
+var castleSecondary
+var castleEquipment
+
+var pulsePrimary
+var pulseSecondary
+var pulseEquipment
+
+var docPrimary
+var docSecondary
+var docEquipment
+
+var rookPrimary
+var rookSecondary
+var rookEquipment
+
+var kapkanPrimary
+var kapkanSecondary
+var kapkanEquipment
+
+var tachankaPrimary
+var tachankaSecondary
+var tachankaEquipment
+
+var jagerPrimary
+var jagerSecondary
+var jagerEquipment
+
+var banditPrimary
+var banditSecondary
+var banditEquipment
+
+var frostPrimary
+var frostSecondary
+var frostEquipment
+
+var valkyriePrimary
+var valkyrieSecondary
+var valkyrieEquipment
+
+var caveiraPrimary
+var caveiraSecondary
+var caveiraEquipment
+
+var echoPrimary
+var echoSecondary
+var echoEquipment
+
+var miraPrimary
+var miraSecondary
+var miraEquipment
+
+var lesionPrimary
+var lesionSecondary
+var lesionEquipment
+
+var elaPrimary
+var elaSecondary
+var elaEquipment
+
+var vigilPrimary
+var vigilSecondary
+var vigilEquipment
+
+
